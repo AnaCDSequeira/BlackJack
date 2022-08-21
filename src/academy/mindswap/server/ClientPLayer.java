8fecc7947e1e3b0dc358cba9032261c0a587009a
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-08-21 18:55:08 +0100

    Create ClientPLayer properties

--- src/academy/mindswap/server/ClientPLayer.java
+++ src/academy/mindswap/server/ClientPLayer.java
@@ -1,7 +1,13 @@
 package academy.mindswap.server;
 
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class ClientPLayer {
 
+    private Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
 }
