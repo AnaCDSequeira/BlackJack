183e8a2b387a745a1e3a2985768217a0c47529cb
MrMotiz <108729772+MrMotiz@users.noreply.github.com>
2022-08-23 15:41:46 +0100

    added some messages and removes unnecessary classes

--- src/academy/mindswap/server/Messages.java
+++ src/academy/mindswap/server/Messages.java
@@ -5,12 +5,12 @@ public class Messages {
     public static final String OPEN_SERVER = "BlackJack is open and accepting players";
     public static final String PLAYER_CONNECTED = "New player arrived, waiting for other players";
     public static final String WELCOME_PLAYER = "" +
-            "                                    ██████╗ ██╗      █████╗  ██████╗██╗  ██╗ ██╗     ██╗ █████╗  ██████╗██╗  ██╗\n" +
-            "                                    ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝██╔╝     ██║██╔══██╗██╔════╝██║ ██╔╝\n" +
-            "                                    ██████╔╝██║     ███████║██║     █████╔╝██╔╝      ██║███████║██║     █████╔╝\n" +
-            "                                    ██╔══██╗██║     ██╔══██║██║     ██╔═██╗╚██╗ ██   ██║██╔══██║██║     ██╔═██╗\n" +
-            "                                    ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗╚██╗╚█████╔╝██║  ██║╚██████╗██║  ██╗\n" +
-            "                                    ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═╝ ╚════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝";
+            "          ██████╗ ██╗      █████╗  ██████╗██╗  ██╗ ██╗     ██╗ █████╗  ██████╗██╗  ██╗\n" +
+            "          ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝██╔╝     ██║██╔══██╗██╔════╝██║ ██╔╝\n" +
+            "          ██████╔╝██║     ███████║██║     █████╔╝██╔╝      ██║███████║██║     █████╔╝\n" +
+            "          ██╔══██╗██║     ██╔══██║██║     ██╔═██╗╚██╗ ██   ██║██╔══██║██║     ██╔═██╗\n" +
+            "          ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗╚██╗╚█████╔╝██║  ██║╚██████╗██║  ██╗\n" +
+            "          ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═╝ ╚════╝ ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝";
     public static final String CHOOSE_USERNAME = "Please choose your username for the game...";
     public static final String WELCOME_USERNAME = "Welcome, %s\n";
     public static final String SPENT_AMOUNT = "And how much will you spend? Minimum is 5 euros.";
